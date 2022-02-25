aa45525219705e6448f671f024867440f7af8268
JoaquinRodriguez2006 <joaquienrodriguez760@gmail.com>
2022-02-25 12:17:33 -0300

    Add Get Rotation method

---
+++ get_rotation.py
@@ -0,0 +1,35 @@
+import math
+import numpy as np
+
+from controller import Robot, GPS
+from controller import Motor
+from controller import PositionSensor
+from controller import Supervisor
+
+supervisor = Supervisor()
+
+R = supervisor.getSelf().getOrientation()
+
+# Checks if a matrix is a valid rotation matrix.
+def isRotationMatrix(R) :
+    Rt = np.transpose(R)
+    shouldBeIdentity = np.dot(Rt, R)
+    I = np.identity(3, dtype = R)
+    n = np.linalg.norm(I - shouldBeIdentity)
+    return n < 1e-6
+
+def rotationMatrixToEulerAngles(R) :
+    assert(isRotationMatrix(R))
+    sy = math.sqrt(R[0,0] * R[0,0] +  R[1,0] * R[1,0])
+    singular = sy < 1e-6
+    if  not singular :
+        x = math.atan2(R[2,1] , R[2,2])
+        y = math.atan2(-R[2,0], sy)
+        z = math.atan2(R[1,0], R[0,0])
+    else :
+        x = math.atan2(-R[1,2], R[1,1])
+        y = math.atan2(-R[2,0], sy)
+        z = 0
+    return np.array([x, y, z])
+
+rotationMatrixToEulerAngles(R)
