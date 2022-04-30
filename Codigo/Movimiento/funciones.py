9747e136c1937f39d96cf65a1e8bf5214413ad08
Juarez Enzo <49879701+enzzo19@users.noreply.github.com>
2022-04-30 18:29:08 -0300

    add funciones

--- Codigo/Movimiento/funciones.py
+++ Codigo/Movimiento/funciones.py
@@ -58,57 +58,33 @@ def rotar(angulo):
     global angulo_actual
     global tiempo_anterior
     #  iniciar_rotacion
-    
     if angulo > 0:
         girar_der(0.5)
-        # Mientras no llego al angulo solicitado sigo girando
-        if (abs(angulo - angulo_actual) > 1):
-            tiempo_actual = robot.getTime()
-            # print("Inicio rotacion angulo", angulo, "Angulo actual:",angulo_actual)
-            tiempo_transcurrido = tiempo_actual - \
-                tiempo_anterior  # tiempo que paso en cada timestep
-            # rad/seg * mseg * 1000
-            radsIntimestep = abs(gyro.getValues()[1]) * tiempo_transcurrido
-            degsIntimestep = radsIntimestep * 180 / math.pi
-            # print("rads: " + str(radsIntimestep) +
-            #    " | degs: " + str(degsIntimestep))
-            angulo_actual += degsIntimestep
-            # Si se pasa de 360 grados se ajusta la rotacion empezando desde 0 grados
-            angulo_actual = angulo_actual % 360
-            # Si es mas bajo que 0 grados, le resta ese valor a 360
-            if angulo_actual < 0:
-                angulo_actual += 360
-            tiempo_anterior = tiempo_actual
-            # print("Angulo actual:", angulo_actual)
-            return False
-        # print("Rotacion finalizada.")
-        angulo_actual = 0
-        return True
     else:
         girar_izq(0.5)
-        # Mientras no llego al angulo solicitado sigo girando
-        if (abs(abs(angulo) - angulo_actual) > 1):
-            tiempo_actual = robot.getTime()
-            # print("Inicio rotacion angulo", angulo, "Angulo actual:",angulo_actual)
-            tiempo_transcurrido = tiempo_actual - \
-                tiempo_anterior  # tiempo que paso en cada timestep
-            # rad/seg * mseg * 1000
-            radsIntimestep = abs(gyro.getValues()[1]) * tiempo_transcurrido
-            degsIntimestep = radsIntimestep * 180 / math.pi
-            print("rads: " + str(radsIntimestep) +
-                " | degs: " + str(degsIntimestep))
-            angulo_actual += degsIntimestep
-            # Si se pasa de 360 grados se ajusta la rotacion empezando desde 0 grados
-            angulo_actual = angulo_actual % 360
-            # Si es mas bajo que 0 grados, le resta ese valor a 360
-            if angulo_actual < 0:
-                angulo_actual += 360
-            tiempo_anterior = tiempo_actual
-            print("Angulo actual:", angulo_actual)
-            return False
-        print("Rotacion finalizada.")
-        angulo_actual = 0
-        return True
+    # Mientras no llego al angulo solicitado sigo girando
+    if (abs(abs(angulo) - angulo_actual) > 1):
+        tiempo_actual = robot.getTime()
+        # print("Inicio rotacion angulo", angulo, "Angulo actual:",angulo_actual)
+        tiempo_transcurrido = tiempo_actual - \
+            tiempo_anterior  # tiempo que paso en cada timestep
+        # rad/seg * mseg * 1000
+        radsIntimestep = abs(gyro.getValues()[1]) * tiempo_transcurrido
+        degsIntimestep = radsIntimestep * 180 / math.pi
+        print("rads: " + str(radsIntimestep) +
+            " | degs: " + str(degsIntimestep))
+        angulo_actual += degsIntimestep
+        # Si se pasa de 360 grados se ajusta la rotacion empezando desde 0 grados
+        angulo_actual = angulo_actual % 360
+        # Si es mas bajo que 0 grados, le resta ese valor a 360
+        if angulo_actual < 0:
+            angulo_actual += 360
+        tiempo_anterior = tiempo_actual
+        print("Angulo actual:", angulo_actual)
+        return False
+    print("Rotacion finalizada.")
+    angulo_actual = 0
+    return True
 
 def delay(ms):
     initTime = robot.getTime()      # Store starting time (in seconds)
@@ -139,11 +115,5 @@ tiempo_anterior = robot.getTime()
 contador = 0
 while robot.step(timeStep) != -1:
     
-    rotar_enclavado(90)
-    avanzar_baldoza()
-    rotar_enclavado(90)
-    avanzar_baldoza()
-    rotar_enclavado(90)
-    avanzar_baldoza()
-    rotar_enclavado(90)
+    rotar_enclavado(-90)
     avanzar_baldoza()
